c8e773f15f707c2b163a5e0eb9be038d86500c3b
Nesh0-0 <ganesan.s99@gmail.com>
2025-08-04 21:27:46 +0530

    changed db credentials

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('test_db', 'root', 'root', {
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
 });
