d6558fcfcb1bbb77799d5d9ba7dd44fe38728269
Nesh0-0 <ganesan.s99@gmail.com>
2025-08-04 21:35:17 +0530

    Changed db.js 2

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: 'localhost',
+    host: process.env.RDS_ENDPOINT,
     dialect: 'mysql'
 });
 
